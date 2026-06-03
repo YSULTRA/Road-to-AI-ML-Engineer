5cebf273992e2077b14293f5dd45d01cdd60f915
YSULTRA <yash22589@iiitd.ac.in>
2026-06-03 20:11:30 +0530

    IO Operations done

---
+++ Python-Revision/IO-operations.py
@@ -0,0 +1,25 @@
+#Output
+
+
+print("Hello How are You")
+
+name = "Yash"
+age  =  "22"
+
+print(name,age)
+
+print("my name is",name," and my age is ",age)
+
+#formatted String
+print(f"my name is {name} and my age is {age}")
+
+#Raw String raw string is a string where escape sequences (like \n, \t, \\) are treated as literal characters, not special characters.
+print("Hello\nWorld")
+print(r"Hello\nWorld")
+
+
+#Taking Inputs
+name = input("Enter Your name please: ")
+age  = int(input("Enter Your age :"))
+
+print(f"Your name is {name} and your age is {age}")
