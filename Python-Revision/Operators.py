ad957261403ac0fd05b6a0457801c0e992433e2d
YSULTRA <yash22589@iiitd.ac.in>
2026-06-04 17:27:21 +0530

    Operators Revision

---
+++ Python-Revision/Operators.py
@@ -0,0 +1,26 @@
+a = 5
+b = 12
+
+
+print( a + b)
+print( a - b)
+print( a * b)
+print( b/a)
+print( a/b)
+
+#Floor Division
+print( b//a)
+print( a//b)
+
+print(b ** a)
+
+print(33%5)
+
+
+#compound operators
+
+
+A = 20
+A += 20
+A -= 20
+print(A)
