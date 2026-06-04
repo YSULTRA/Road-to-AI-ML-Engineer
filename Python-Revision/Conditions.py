9fc23a4f1c95dbdcb5bad3302c46f7623ed38d36
YSULTRA <yash22589@iiitd.ac.in>
2026-06-04 18:46:00 +0530

    Conditios

---
+++ Python-Revision/Conditions.py
@@ -0,0 +1,31 @@
+# age = int(input("Enter your age:"))
+
+# if age >= 18 :
+#     print("You are eligible to vote")
+# else:
+#     print("You are not eligible to vote")
+
+
+#Q1
+# numberOne = int(input("Enter first number:"))
+# numberTwo = int(input("Enter second number:"))
+
+# if numberOne == numberTwo:
+#     print("Both are Equal")
+# elif numberOne > numberTwo:
+#     print(f"number one is greater,{numberOne}")
+# else:
+#     print(f"number two is greater,{numberTwo}")
+
+
+# year = int(input("Enter Year here: "))
+
+# if year%400 == 0:
+
+#     print("Leap Year")
+# else:
+
+#     if year%4 == 0 and year%100 != 0:
+#         print("Leap Year")
+#     else:
+#         print("Not Leap Year") 
